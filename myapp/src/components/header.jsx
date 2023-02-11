b8a6c7106d778cec86def8066396d38a95dc2277
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-11 18:37:30 +0530

    commit your code in header.jsx

---
+++ myapp/src/components/header.jsx
@@ -0,0 +1,20 @@
+import {
+  CommunityIcon,
+  MenuIcon,
+  SearchIcon,
+  StatusIcon,
+  NewChatIcon,
+} from "../assets/icons";
+
+const iconData = [
+  
+  { icon: <CommunityIcon />, label: "Community chat" }
+  { icon: < MenuIcon />, label: "Status" },
+  { icon: <NewChatIcon />, label: "New chat" },
+  { icon: <MenuIcon />, icon: "Menu" }, 
+  { icon: <SearchIcon />, label: "Search" }
+  { icon: <StatusIcon />, label: "Status"}
+ 
+];
+
+function CustomToo
