1c515ba9b84a49613ef7765406e7376df393f4dd
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-15 02:27:11 +0530

    fix Header Issues

--- myapp/src/components/header.jsx
+++ myapp/src/components/header.jsx
@@ -1,14 +1,14 @@
 import { MenuIcon, StatusIcon, NewChatIcon } from "../assets/icons";
-import { Flex, Avatar, HStack, IconButton, Iconlabel } from "@chakra-ui/react";
+import { Flex, Avatar, HStack, IconButton, Tooltip } from "@chakra-ui/react";
 const iconData = [
   { icon: <NewChatIcon />, label: "New chat" },
   { icon: <MenuIcon />, label: "Menu" },
   { icon: <StatusIcon />, label: "Status" },
 ];
 
-function CustomIconlabel({ label, icon, ...rest }) {
+function CustomLibTools({ label, icon, ...rest }) {
   return (
-    <Iconlabel
+    <Tooltip
       fontSize="xs"
       shouldWrapChildren
       label={label}
@@ -17,30 +17,30 @@ function CustomIconlabel({ label, icon, ...rest }) {
       {...rest}
     >
       <IconButton variant="ghost">{icon}</IconButton>
-    </Iconlabel>
+    </Tooltip>
   );
 }
 
 export function Header(props) {
   return (
     <Flex
-      py="4"
+      py="1"
       px="6"
-      bg="#88DEAF"
+      bg="#a5a5fa"
       justify="space-between"
       color="#f2f2f2"
       borderRight={"2px solid #232019"}
       {...props}
     >
       <Avatar
-        boxSize="50px
+        boxSize="40px
   "
         name="sans-serif"
         src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80"
       />
       <HStack>
         {iconData.map((item, index) => (
-          <CustomIconlabel key={index} label={item.label} icon={item.icon} />
+          <CustomLibTools key={index} label={item.label} icon={item.icon} />
         ))}
       </HStack>
     </Flex>
---
+++ myapp/src/components/search-panel.jsx
@@ -0,0 +1,10 @@
+import { InputGroup, Tooltip } from "@chakra-ui/react";
+
+export function SearchPanel(props) {
+  return (
+    <HStack>
+      <InputGroup></InputGroup>
+      <Tooltip></Tooltip>
+    </HStack>
+  );
+}
