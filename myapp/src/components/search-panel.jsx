53595d6f7718e9da4d4f7c0cbfb0b65e457b36ad
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-16 17:55:18 +0530

    add search on chat group

--- myapp/src/components/search-panel.jsx
+++ myapp/src/components/search-panel.jsx
@@ -1,10 +1,33 @@
-import { InputGroup, Tooltip } from "@chakra-ui/react";
+import {
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Tooltip,
+} from "@chakra-ui/react";
 
 export function SearchPanel(props) {
   return (
     <HStack>
-      <InputGroup></InputGroup>
-      <Tooltip></Tooltip>
+      <InputGroup>
+        <InputLeftElement />
+
+        <Input
+          _placeholder={{
+            opacity: 0.6,
+            color: "#8d80d9",
+            paddingleft: "30px",
+            fontSize: "10px",
+          }}
+          h="36px"
+          bg="#cbcfd4"
+          variant="filled"
+          placeholder="Search for Friends..."
+        />
+      </InputGroup>
+      <Tooltip>
+        <IconButton></IconButton>
+      </Tooltip>
     </HStack>
   );
 }
