d53b7680f3d5eb09d47dc74185b4d505d399d45f
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-16 23:36:59 +0530

    add search bar on top

--- myapp/src/components/search-panel.jsx
+++ myapp/src/components/search-panel.jsx
@@ -4,29 +4,40 @@ import {
   InputGroup,
   InputLeftElement,
   Tooltip,
+  FilterIcon,
+  IconButton,
 } from "@chakra-ui/react";
 
 export function SearchPanel(props) {
   return (
-    <HStack>
+    <HStack spacing={"2.5"} px="8" py="2" borderRadius={"1px"}>
       <InputGroup>
-        <InputLeftElement />
+        <InputLeftElement pointerEvents={"n"} />
 
         <Input
           _placeholder={{
-            opacity: 0.6,
+            opacity: 0.88,
             color: "#8d80d9",
             paddingleft: "30px",
             fontSize: "10px",
           }}
           h="36px"
+          _hover={{ bg: "#f0f2f5  " }}
           bg="#cbcfd4"
           variant="filled"
-          placeholder="Search for Friends..."
+          placeholder="Search Your Friends..."
         />
       </InputGroup>
-      <Tooltip>
-        <IconButton></IconButton>
+      <Tooltip
+        shouldWrapChildren
+        label="Unread Chats Filter"
+        bg="#8d80d9"
+        color="black"
+        fontSize={"md"}
+      >
+        <IconButton>
+          <FilterIcon />
+        </IconButton>
       </Tooltip>
     </HStack>
   );
