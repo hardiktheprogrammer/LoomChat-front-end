0e9150244303a18d9795dce976d8c6982f131df7
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-17 07:32:45 +0530

    add Chat list component file

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Header } from "./components/header";
+import { Flex } from "@chakra-ui/react";
+
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Flex>
+      <LeftPannel />
+      <RightPannel />
+    </Flex>
   );
 }
 
--- myapp/src/components/chatlist.jsx
+++ myapp/src/components/chatlist.jsx
@@ -1,31 +1,21 @@
-import {Avatar,Flex,HStack} from "@chakra-ui/react"
+import { Avatar, Flex, HStack } from "@chakra-ui/react";
 
-export function Chat({ name, message, date, seen, src, ...rest }) {
+export function Chat({ name, message, date, seen, mainsrc, ...rest }) {
+  return (
+    <HStack>
+      <Avatar mx="3" name={name} src={mainsrc} />
 
-    return (
-
-        <HStack>
-            <Avatar>
-
-                <Box>
-                    <Flex>
-                    <Box>
-                            <Text> </Text>
-                                <HStack>
-
-                                <DeliveredIcon />
-                                <Test></Test>
-                            </HStack>
-                            
-                            
-                    </Box>
-            
-                </Flex>
-            
-        </Box>
-    </HStack >
-
-);
+      <Box flex="4" pr="6">
+        <Flex justifyContent={"space-between"} align={"baseline"}>
+          <Box>
+            <Text fontWeight="300"> {name}</Text>
+            <HStack>
+              <DeliveredIcon />
+              <Test></Test>
+            </HStack>
+          </Box>
+        </Flex>
+      </Box>
+    </HStack>
+  );
 }
-    
-    
